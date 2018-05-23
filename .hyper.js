0bc80c926ade9fc8423071f461f442be9b267a23
Daniel Heene <heene@d-sire.com>
2018-05-23 16:43:00 +0200

    changed hyper.js

--- .hyper.js
+++ .hyper.js
@@ -1,48 +1,16 @@
 module.exports = {
   config: {
     updateChannel: 'stable',
-    fontSize: 13,
+    fontSize: 14  ,
     fontFamily: '"Monaco for Powerline", Menlo, monospace',
-
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,1)',
-
-    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
     cursorShape: 'UNDERLINE',
-
-    // set to true for blinking cursor
-    cursorBlink: false,
-
-    // color of the text
+    cursorBlink: true,
     foregroundColor: '#fff',
-
-    // terminal background color
     backgroundColor: '#000',
-
-    // border color (window, tabs)
     borderColor: '#333',
-
-    // custom css to embed in the main window
-    css: '',
-
-    // custom css to embed in the terminal window
-    termCSS: '',
-
-    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
-    // default: `false` on Linux, `true` on Windows (ignored on macOS)
-    showHamburgerMenu: '',
-
-    // set to `false` if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` on windows and Linux (ignored on macOS)
-    showWindowControls: '',
-
-    // custom padding (css format, i.e.: `top right bottom left`)
     padding: '0 10px',
 
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
     colors: {
       black: '#000000',
       red: '#ff0000',
@@ -59,60 +27,24 @@ module.exports = {
       lightBlue: '#0066ff',
       lightMagenta: '#cc00ff',
       lightCyan: '#00ffff',
-      lightWhite: '#ffffff'
+      lightWhite: '#ffffff',
     },
 
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    //
-    // Windows
-    // - Make sure to use a full path if the binary name doesn't work
-    // - Remove `--login` in shellArgs
-    //
-    // Bash on Windows
-    // - Example: `C:\\Windows\\System32\\bash.exe`
-    //
-    // Powershell on Windows
-    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
     shell: '',
-
-    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-    // by default ['--login'] will be used
     shellArgs: ['--login'],
-
-    // for environment variables
     env: {},
-
-    // set to false for no bell
-    bell: 'SOUND',
-
-    // if true, selected text will automatically be copied to the clipboard
+    bell: false,
     copyOnSelect: false,
 
-    // if true, on right click selected text will be copied or pasted if no
-    // selection is present (true by default on Windows)
-    // quickEdit: true
-
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
-
-    // for advanced config flags please refer to https://hyper.is/#cfg
     hyperBorder: {
       animate: {
         duration: '3s',
       },
       borderWidth: '2px',
-    }
+    },
   },
-
+  
   plugins: [
-    'hyperborder'
+    'hyperborder',
   ],
-
-  localPlugins: [],
-
-  keymaps: {
-    // Example
-    // 'window:devtools': 'cmd+alt+o',
-  },
 };
