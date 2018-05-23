1b0a4514afbf1e6f8737aaba10c50e20ecd82d64
Daniel Heene <heene@d-sire.com>
2018-05-23 14:01:29 +0200

    changed hyper.js

--- .hyper.js
+++ .hyper.js
@@ -109,9 +109,6 @@ module.exports = {
     'hyperborder'
   ],
 
-  // in development, you can create a directory under
-  // `~/.hyper_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
   localPlugins: [],
 
   keymaps: {
